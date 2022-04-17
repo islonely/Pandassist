ba34ef4b22b3869073980b1f06186be0d11d04e5
Adam Oates <31167933+islonely@users.noreply.github.com>
2022-04-17 16:44:43 -0500

    util.v: io.v consolidated to util.v

--- util.v
+++ util.v
@@ -1,7 +1,40 @@
 module main
 
 import crypto.bcrypt
+import os
 import time
+import toml
+
+const(
+	const_config_path = './config.toml'
+	const_default_conf = 
+	'
+	port = 80
+	
+	[mysql]
+	dbname = "pandassist"
+	host = "127.0.0.1"
+	port = 3306
+	username = ""
+	password = ""
+	'
+)
+
+// parse_toml reads the toml file at the provided path if it exists
+// and parses the contents.
+fn parse_toml(path string) toml.Doc {
+	if !os.exists(path) || !os.is_file(path) {
+		os.write_file(path, const_default_conf) or {
+			println_error(errors[errno_no_toml_config])
+			exit(0)
+		}
+	}
+	doc := toml.parse_file(path) or {
+		println_error(errors[errno_no_toml_config])
+		exit(0)
+	}
+	return doc
+}
 
 // calc_ideal_bcrypt_cost automatically determines what the optimal cost for the
 // bcrypt algorithm should be. This scales with hardware. So as processors get more
